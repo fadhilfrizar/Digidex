4640559856b0355c2bee14649c0eafbf5bd0687a
fadhilfrizar <56080817+fadhilfrizar@users.noreply.github.com>
2026-01-06 20:41:33 +0700

    Update UIImageView+Helper.swift

--- Digidex/Helpers/UIImageView+Helper.swift
+++ Digidex/Helpers/UIImageView+Helper.swift
@@ -2,47 +2,21 @@
 //  UIImageView+Helper.swift
 //  RickAndMorty
 //
-//  Created by USER-MAC-GLIT-007 on 17/02/23.
+//  Simple image loader (no extra pods required)
 //
 
-import Foundation
 import UIKit
 
-let imageCache = NSCache<AnyObject, AnyObject>()
-
 extension UIImageView {
-    
-    func loadImageUsingCacheWithUrlString(_ urlString: String) {
-        
-        self.image = nil
-        
-        //check cache for image first
-        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
-            self.image = cachedImage
-            return
-        }
-        
-        //otherwise fire off a new download
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-            
-            //download hit an error so lets return out
-            if error != nil {
-                print(error ?? "")
-                return
+    func loadImage(from urlString: String?) {
+        self.image = UIImage(systemName: "photo")
+        guard let urlString, let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            guard let self, let data, let img = UIImage(data: data) else { return }
+            DispatchQueue.main.async { [weak self] in
+                self?.image = img
             }
-            
-            DispatchQueue.main.async(execute: {
-                
-                if let downloadedImage = UIImage(data: data!) {
-                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
-                    
-                    self.image = downloadedImage
-                }
-            })
-            
-        }).resume()
+        }.resume()
     }
-    
 }

72332d8146c48146522c3beb5de8297cf3367f29
fadhilfrizar <56080817+fadhilfrizar@users.noreply.github.com>
2026-01-06 20:34:41 +0700

    Update CharacterEndpoint.swift

--- Digidex/Characters/Service/CharacterEndpoint.swift
+++ Digidex/Characters/Service/CharacterEndpoint.swift
@@ -2,26 +2,19 @@
 //  CharacterEndpoint.swift
 //  RickAndMorty
 //
-//  Created by USER-MAC-GLIT-007 on 16/02/23.
-//
 
 import Foundation
 
-public enum CharacterEndpoint {
-    
-    case get(page: Int)
-    
-    public func url(baseURL: URL) -> URL {
+enum CharacterEndpoint {
+    case get(page: Int, pageSize: Int)
+    case detail(id: Int)
+
+    func url(baseURL: URL) -> URL {
         switch self {
-        case .get(_):
-            var components = URLComponents()
-            components.scheme = baseURL.scheme
-            components.host = baseURL.host
-            components.path = baseURL.path + "/character"
-//            components.queryItems = [
-//                URLQueryItem(name: "page", value: "\(page)")
-//            ].compactMap { $0 }
-            return components.url!
+        case .get:
+            return baseURL.appendingPathComponent("digimon")
+        case .detail(let id):
+            return baseURL.appendingPathComponent("digimon/\(id)")
         }
     }
 }
